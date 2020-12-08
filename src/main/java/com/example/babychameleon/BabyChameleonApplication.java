534f3a165b3c627b99b1c016a43a7f13fd690559
Björn Hallberg <bjorn.hallberg@hm.com>
2020-12-08 15:33:59 +0100

    Added files with upper case

---
+++ src/main/java/com/example/babychameleon/BabyChameleonApplication.java
@@ -0,0 +1,13 @@
+package com.example.babychameleon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyChameleonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyChameleonApplication.class, args);
+	}
+
+}
