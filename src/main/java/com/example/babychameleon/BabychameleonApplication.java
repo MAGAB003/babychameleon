4fa506b3d1ef241b5229c3936b203c6bbe82dfc7
HM\MAGAO <maja.gabrielsson@hm.com>
2020-12-08 15:18:04 +0100

    first commit

---
+++ src/main/java/com/example/babychameleon/BabychameleonApplication.java
@@ -0,0 +1,13 @@
+package com.example.babychameleon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabychameleonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabychameleonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/babychameleon/BabychameleonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.babychameleon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabychameleonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
