12f464c5d8e52a01c6f41ad8bf2ce4ab704a8277
HM\MAGAO <maja.gabrielsson@hm.com>
2020-12-09 10:07:06 +0100

    created customer class

---
+++ src/main/java/com/example/babychameleon/Customer.java
@@ -0,0 +1,105 @@
+package com.example.babychameleon;
+
+public class Customer {
+
+    private String customerID;
+    private int customerNumber;
+    private String customerFirstName;
+    private String customerLastName;
+    private String customerEmail;
+    private String address;
+    private int postalCode;
+    private int city;
+    private int creditCardNumber;
+    private String creditCardExpirationDate;
+    private int cvvCode;
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getCustomerFirstName() {
+        return customerFirstName;
+    }
+
+    public void setCustomerFirstName(String customerFirstName) {
+        this.customerFirstName = customerFirstName;
+    }
+
+    public String getCustomerLastName() {
+        return customerLastName;
+    }
+
+    public void setCustomerLastName(String customerLastName) {
+        this.customerLastName = customerLastName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public int getCity() {
+        return city;
+    }
+
+    public void setCity(int city) {
+        this.city = city;
+    }
+
+    public int getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(int creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    public String getCreditCardExpirationDate() {
+        return creditCardExpirationDate;
+    }
+
+    public void setCreditCardExpirationDate(String creditCardExpirationDate) {
+        this.creditCardExpirationDate = creditCardExpirationDate;
+    }
+
+    public int getCvvCode() {
+        return cvvCode;
+    }
+
+    public void setCvvCode(int cvvCode) {
+        this.cvvCode = cvvCode;
+    }
+
+}
