52f17d0c65f9e4ce47af925e7009eac3f9ddc6b7
Björn Hallberg <bjorn.hallberg@hm.com>
2020-12-09 11:24:20 +0100

    Added home page with menu and style template

--- src/main/java/com/example/babychameleon/BabyChameleonController.java
+++ src/main/java/com/example/babychameleon/BabyChameleonController.java
@@ -1,7 +1,18 @@
 package com.example.babychameleon;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class BabyChameleonController {
+
+    @GetMapping
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/aboutservice")
+    public String aboutservice() {
+        return "aboutservice";
+    }
 }
