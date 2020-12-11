b07f6e0b2e61b61411ea7029b73cb6d9bcb22654
HM\MAMIS <maria.mickols@hm.com>
2020-12-11 14:19:21 +0100

    Added img1-4 as attribute to subscription class + data + schema

--- src/main/java/com/example/babychameleon/Subscription.java
+++ src/main/java/com/example/babychameleon/Subscription.java
@@ -11,7 +11,10 @@ public class Subscription {
     private Long ID;
     private String name;
     private String description;
-    private String imgID;
+    private String img1;
+    private String img2;
+    private String img3;
+    private String img4;
     @Column(name="Age_Group")
     private String ageGroup;
     private String theme;
@@ -20,10 +23,13 @@ public class Subscription {
     public Subscription(){
     }
 
-    public Subscription(String name, String description, String imgID, String ageGroup, String theme){
+    public Subscription(String name, String description, String img1, String img2, String img3, String img4, String ageGroup, String theme){
     this.name =name;
     this.description=description;
-    this.imgID=imgID;
+    this.img1=img1;
+    this.img2=img2;
+    this.img3=img3;
+    this.img4=img4;
     this.ageGroup=ageGroup;
     this.theme=theme;
     }
@@ -52,12 +58,36 @@ public class Subscription {
         this.description = description;
     }
 
-    public String getImgID() {
-        return imgID;
+    public String getImg1() {
+        return img1;
     }
 
-    public void setImgID(String imgID) {
-        this.imgID = imgID;
+    public void setImg1(String img1) {
+        this.img1 = img1;
+    }
+
+    public String getImg2() {
+        return img2;
+    }
+
+    public void setImg2(String img2) {
+        this.img2 = img2;
+    }
+
+    public String getImg3() {
+        return img3;
+    }
+
+    public void setImg3(String img3) {
+        this.img3 = img3;
+    }
+
+    public String getImg4() {
+        return img4;
+    }
+
+    public void setImg4(String img4) {
+        this.img4 = img4;
     }
 
     public String getAgeGroup() {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO SUBSCRIPTION (Name, Description, ImgID, Age_Group, Theme, Price) VALUES ('Complete package', 'Everything that your child will need', '/img/subscriptions/sub_baby.jpg', 'BABY', 'standard', 300);
-INSERT INTO SUBSCRIPTION (Name, Description, ImgID, Age_Group, Theme, Price) VALUES ('Small package', 'Only the most necessary', '/img/subscriptions/sub_kids.jpg', 'KIDS', 'standard', 200);
+INSERT INTO SUBSCRIPTION (Name, Description, Img1, Img2, Img3, Img4, Age_Group, Theme, Price) VALUES ('Small package', 'Only the most necessary', '/img/subscriptions/newborn_small_1.jpg', '/img/subscriptions/newborn_small_2.jpg', '/img/subscriptions/newborn_small_3.jpg', '/img/subscriptions/newborn_small_4.jpg', 'NEWBORN', 'standard', 200);
+INSERT INTO SUBSCRIPTION (Name, Description, Img1, Img2, Img3, Img4, Age_Group, Theme, Price) VALUES ('Complete package', 'Everything that your child will need', '/img/subscriptions/baby_complete_1.jpg','/img/subscriptions/baby_complete_2.jpg','/img/subscriptions/baby_complete_3.jpg','/img/subscriptions/baby_complete_4.jpg', 'BABY', 'standard', 300);
 
 INSERT INTO CUSTOMER (CustomerNumber, FirstName, LastName, Email, StreetAddress, PostalCode, City) VALUES (1, 'Pippi', 'Långstrump', 'pippi@langstrump.com', 'Villavillekullavägen 2', '11111', 'LindgrenCity');
 INSERT INTO CUSTOMER (CustomerNumber, FirstName, LastName, Email, StreetAddress, PostalCode, City) VALUES (2,'Emil', 'Jansson', 'emil@jansson.com', 'lönneberga 2', '222222', ' Lönneberga');
