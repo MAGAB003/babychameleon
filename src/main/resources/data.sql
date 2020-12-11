f513008c1638b3bd089e4035c5e1deab4a80f845
HM\MAMIS <maria.mickols@hm.com>
2020-12-11 11:19:28 +0100

    Added test data for image on imgID

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO SUBSCRIPTION (Name, Description, ImgID, Age_Group, Theme, Price) VALUES ('Complete package', 'Everything that your child will need', 'bild.img', 'BABY', 'standard', 300);
-INSERT INTO SUBSCRIPTION (Name, Description, ImgID, Age_Group, Theme, Price) VALUES ('Small package', 'Only the most necessary', 'bild2.img', 'KIDS', 'standard', 200);
+INSERT INTO SUBSCRIPTION (Name, Description, ImgID, Age_Group, Theme, Price) VALUES ('Complete package', 'Everything that your child will need', '/img/subscriptions/sub_baby.jpg', 'BABY', 'standard', 300);
+INSERT INTO SUBSCRIPTION (Name, Description, ImgID, Age_Group, Theme, Price) VALUES ('Small package', 'Only the most necessary', '/img/subscriptions/sub_kids.jpg', 'KIDS', 'standard', 200);
 
 INSERT INTO CUSTOMER (CustomerNumber, FirstName, LastName, Email, StreetAddress, PostalCode, City) VALUES (1, 'Pippi', 'Långstrump', 'pippi@langstrump.com', 'Villavillekullavägen 2', '11111', 'LindgrenCity');
 INSERT INTO CUSTOMER (CustomerNumber, FirstName, LastName, Email, StreetAddress, PostalCode, City) VALUES (2,'Emil', 'Jansson', 'emil@jansson.com', 'lönneberga 2', '222222', ' Lönneberga');
