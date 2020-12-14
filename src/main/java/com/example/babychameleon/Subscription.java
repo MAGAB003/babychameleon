398ffd30f925e26b151ba98abe130102c60953ab
HM\MAMIS <maria.mickols@hm.com>
2020-12-14 17:58:52 +0100

    Added getters and setters for price

--- src/main/java/com/example/babychameleon/Subscription.java
+++ src/main/java/com/example/babychameleon/Subscription.java
@@ -21,7 +21,7 @@ public class Subscription {
     public Subscription() {
     }
 
-    public Subscription(String name, String description, String img1, String img2, String img3, String img4, String ageGroup, String theme){
+    public Subscription(String name, String description, String img1, String img2, String img3, String img4, String ageGroup, String theme, int price){
     this.name =name;
     this.description=description;
     this.img1=img1;
@@ -30,6 +30,7 @@ public class Subscription {
     this.img4=img4;
     this.ageGroup=ageGroup;
     this.theme=theme;
+    this.price=price;
     }
 
     public Long getId() {
