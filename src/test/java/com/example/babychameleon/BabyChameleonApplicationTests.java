d3a159a51a331e74e6080ff2c1ba68115faefc2e
HM\MAMIS <maria.mickols@hm.com>
2020-12-15 16:06:52 +0100

    Added test scenario

--- src/test/java/com/example/babychameleon/BabyChameleonApplicationTests.java
+++ src/test/java/com/example/babychameleon/BabyChameleonApplicationTests.java
@@ -14,8 +14,10 @@ class BabyChameleonApplicationTests {
 	@Test public void calculateSubscriptionSum(){
 		Cart cart = new Cart();
 		Subscription subscription = new Subscription("Name1", "Description1", "Img1", "Img2", "Img3", "Img4", "AgeGroup", "Theme", 299);
+		Subscription subscription2 = new Subscription("Name1", "Description1", "Img1", "Img2", "Img3", "Img4", "AgeGroup", "Theme", 99);
 		cart.addToCart(new Child(subscription));
-		Assert.assertEquals(299, cart.calculateSum());
+		cart.addToCart(new Child(subscription2));
+		Assert.assertEquals(398, cart.calculateSum());
 	}
 
 }
