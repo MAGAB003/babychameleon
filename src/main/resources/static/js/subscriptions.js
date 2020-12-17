edef31b786164e57203b5ba20c5249342e49b76f
Björn Hallberg <bjorn.hallberg@hm.com>
2020-12-17 16:38:16 +0100

    Styling of subheader and subscriptions page

--- src/main/resources/static/js/subscriptions.js
+++ src/main/resources/static/js/subscriptions.js
@@ -1,29 +1,43 @@
 $(document).ready(function()
 {
   $("#all").click(function(){
-  console.log("ALL SHOULD SHOW");
+  //console.log("ALL SHOULD SHOW");
     $(".NEWBORN").show();
     $(".BABY").show();
     $(".KIDS").show();
-});
+    $("#newborn").removeClass("active");
+    $("#baby").removeClass("active");
+    $("#kids").removeClass("active");
+    $("#all").addClass("active");
+  });
 
   $("#newborn").click(function(){
     $(".NEWBORN").show();
     $(".BABY").hide();
     $(".KIDS").hide();
+    $("#newborn").addClass("active");
+    $("#baby").removeClass("active");
+    $("#kids").removeClass("active");
+    $("#all").removeClass("active");
   });
 
   $("#baby").click(function(){
   $(".NEWBORN").hide();
     $(".BABY").show();
     $(".KIDS").hide();
-
+    $("#newborn").removeClass("active");
+    $("#baby").addClass("active");
+    $("#kids").removeClass("active");
+    $("#all").removeClass("active");
   });
 
   $("#kids").click(function(){
     $(".NEWBORN").hide();
     $(".BABY").hide();
     $(".KIDS").show();
-
+    $("#newborn").removeClass("active");
+    $("#baby").removeClass("active");
+    $("#kids").addClass("active");
+    $("#all").removeClass("active");
   });
  });
